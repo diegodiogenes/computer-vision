2c537f481f516eeb0f5a0a7847afe60c6779e360
Diego Diogenes <diegodiogenes@iMac-de-Cefas.local>
2020-03-09 18:12:58 -0300

    atualizando

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# computer-vision
+# Visao Computacional
+
+## Tarefa 1
+
+#### 1. Descreva a distribuição de células fotorreceptoras na retina e seu impacto na percepção visual
+
+##### As celulas fotorreceptoras podem ser divididas em dois tipos: Cones e Bastonetes, tais nomes sao de acordo com os formates que possuem. A maior concentracao de cones esta na Macula, regiao da retina 
--- display.py
+++ display.py
@@ -2,9 +2,10 @@ import cv2
 import numpy as np
 import argparse
 
-#função para ler a imagem passada pelo argumento
+# função para ler a imagem passada pelo argumento
 def read_image(image):
     return cv2.imread(image)
+
 
 # função para separar apenas o canal vermelho
 # setamos o azul e o verde para 0
@@ -14,52 +15,58 @@ def channel_r(image):
     r[:, :, 1] = 0
     return r
 
+# função para separar apenas o canal verde
+# setamos o azul e o vermelho para 0
 def channel_g(image):
     g = image.copy()
     g[:, :, 0] = 0
     g[:, :, 2] = 0
     return g
 
+# função para separar apenas o canal azul
+# setamos o vermelho e o verde para 0
 def channel_b(image):
     b = image.copy()
     b[:, :, 1] = 0
     b[:, :, 2] = 0
     return b
 
-#podemos também utilizar o método split do próprio opencv
+# podemos também utilizar o método split do próprio opencv
 def split_channels(image):
     b, g, r = cv2.split(image)
-    return r, g , b
+    return r, g, b
 
+# invertendo imagem horizontalmente
 def horizontal_inv(image):
-    return img[:,::-1]
+    return img[:, ::-1]
 
+# aplicando o blend entre as imagens
 def blend_images(image1, image2):
-    res_img = cv2.resize(image1, (300,300))
-    res_img1 = cv2.resize(image2, (300,300))
-    dst = cv2.addWeighted(res_img, 0.5, res_img1, 0.5, 0.0)
+    res_img = cv2.resize(image1, (300, 300))
+    res_img1 = cv2.resize(image2, (300, 300))
+    blend = np.multiply(0.5, res_img) + np.multiply(0.5, res_img1)
 
-    # save the output image
-    cv2.imwrite('image.png', dst)
-    cv2.imshow('image.png', np.multiply(0.5, res_img))
-    return np.multiply(0.5, res_img)
+    return blend.astype('uint8')
+
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", required=True, help="Diretório/Nome da imagem")
     args = vars(ap.parse_args())
 
+    # lendo as duas imagens
     img = read_image(args["image"])
     img2 = cv2.imread("teste2.jpg")
-    
+
+    # separando cada canal
+    r, g, b = split_channels(img)
+
+    # invertendo horizontalmente
+    inv = horizontal_inv(img)
+
+    # realizando o blend
     blend = blend_images(img, img2)
 
-    r, g, b = split_channels(img)
-    
-    bl = channel_b(img)
-    
-    inv = horizontal_inv(img)
-    
-    # cv2.imshow("imagem", np.multiply(0.5, img2))
+    cv2.imshow("imagem", blend)
 
     cv2.waitKey(0)
